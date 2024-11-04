69866ebcd072c55b3076b9bd4fb9c48c521264cc
Graceatkirk <grace@kirk.org>
2024-11-04 16:47:18 -0600

    added front end stuff and initial css. have some bumps to smooth out

--- calendar-project/client/vite.config.ts
+++ calendar-project/client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()], // Add the React plugin here
   server: {
     port: 3000,
     open: true,
